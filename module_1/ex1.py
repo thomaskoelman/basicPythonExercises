f829968527558db5269857a6286b6ee26ee37aa2
Thomas Koelman <thomaskoelman@hotmail.com>
2022-05-18 23:58:04 +0200

    Create ex1.py

---
+++ module_1/ex1.py
@@ -0,0 +1,2 @@
+s = "Twinkle, twinkle, little star,\n" + 2 * "\t" + "How I wonder what you are!\n" + 4 * "\t" + "Up above the world so high,\n" + 4 * "\t" + "like a diamond in the sky.\n" + "Twinkle, twinkle, little star,\n" + 2 * "\t" + "How I wonder what you are"
+print(s)
